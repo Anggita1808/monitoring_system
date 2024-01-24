0de867cfaa07421c1987b12d435464503c472391
Raacdh03 <citramutia2803@gmail.com>
2024-01-24 12:19:38 +0700

    menambahkan apps.py

--- monitoring_system/settings.py
+++ monitoring_system/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'monitoring_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'monitoring_system',
+        'USER': 'user',
+        'PASSWORD': 'user123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
